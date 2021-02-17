041d3c1a0744b0f024a2c7babbe62380b14bd7e4
Heronnes Pereira <heronnestg@hotmail.com>
2021-02-16 22:01:24 -0300

    redux 007

--- 001_app/src/components/Calculator.jsx
+++ 001_app/src/components/Calculator.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { sum } from '../store/Calculator/Calculator.actions'
 
@@ -7,16 +7,17 @@ import { sum } from '../store/Calculator/Calculator.actions'
 function Calculator() {
      const dispatch = useDispatch()
      const result = useSelector((state) => state.calculator)
-
+     const [a, setA] = useState(0)
+     const [b, setB] = useState(0)
      
 
      return (
          <div>
-             <input type="text" placeholder="a" />
-             <input type="text" placeholder="b" />
+             <input type="text" placeholder="a" value={a} onChange={(evento) => setA(evento.target.value)} />
+             <input type="text" placeholder="b" value={b} onChange={(evento) => setB(evento.target.value)} />
 
 
-             <button onClick={() => { dispatch( sum(4, 7) ) }} >somar</button>
+             <button onClick={() => { dispatch( sum(a, b) ) }} >somar</button>
              <button>subtrarir</button>
 
              <div>
